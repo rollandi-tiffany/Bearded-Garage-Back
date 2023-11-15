00324a3a4fd98f6f2d6dc08579c84e41467ae0bc
Tiffany Torres <torresrtiffany@gmail.com>
2023-11-15 04:10:15 -0800

    update serializers

--- capstoneback/urls.py
+++ capstoneback/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('customers/', include('customers.urls'))
+    path('customers/', include('customers.urls')),
+    path('auth/', obtain_auth_token)
 ]
