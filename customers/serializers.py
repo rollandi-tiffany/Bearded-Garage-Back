3e543b132e7492529c3a4c5d81d8ef90117d04ee
Tiffany Torres <torresrtiffany@gmail.com>
2023-11-17 20:22:56 -0800

    add serializers

--- customers/serializers.py
+++ customers/serializers.py
@@ -1,19 +1,35 @@
 from rest_framework import serializers
-from django.contrib.auth.models import User
-from rest_framework.authtoken.models import Token
+from django.contrib.auth import get_user_model, auth
+#from rest_framework.authtoken.models import Token
 from .models import Vehicle
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta: 
-        model = User
-        fields = ['id', 'username', 'password']
-        extra_kwargs = {'password': {'write_only': True, 'required': True}}
+UserModel = get_user_model()
 
-    def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
-        Token.objects.create(user=user)
-        return user  
+class UserSignupSerializer(serializers.ModelSerializer):
+    class Meta: 
+        model = UserModel
+        fields = "__all__"
+    def create(self, clean_data):
+		user_obj = UserModel.objects.create_user(email=clean_data['email'], password=clean_data['password'])
+		user_obj.username = clean_data['username']
+		user_obj.save()
+		return user_obj
     
+class UserLoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField()
+
+    def check_user(self, clean_data):
+         user = auth(username = clean_data["email"], password = clean_data["password"])
+         if not user:
+              raise ValidationError("USER NOT FOUND")
+         return user
+         
+class UserSerializer(serializers.ModelSerializer):
+     class Meta:
+          model = UserModel
+          fields = ("email", "username")    
+          
 class VehicleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Vehicle
